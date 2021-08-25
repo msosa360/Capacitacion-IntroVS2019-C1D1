045dcb7b9619b279740dd5c7e5df623ef0128571
Maximiliano Sosa <msosa_@hotmail.com>
2021-08-25 17:02:01 -0300

    Add project files.

---
+++ Consola_Introduccion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Consola_Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
